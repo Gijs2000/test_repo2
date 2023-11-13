a8127d461ff1c44622c430085111c9e99ff5e008
Gijs van Male <g.van.male@student.rug.nl>
2023-11-13 12:20:45 +0100

    figure 1 produced plus restore added

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -1,9 +1,9 @@
 #my first github synced script
+library(tidyverse)
 library(renv)
 #restore the versions of the packages used to develop this project
 renv::restore
-
-#renv:init()
+#remember to use oull in Git tab to get changes from github
 
 
 x = c(1,2,3,4,5)
@@ -11,4 +11,8 @@ y = c(1,4,3,6,9)
 data = data.frame (x,y)
 data
 
-fig01= 
+fig001 = data |> ggplot(aes(x = x, y = y)) + 
+  geom_point() +
+  geom_smooth(method = "lm")
+
+ggsave("./figures/fig001.png", plot = fig001, width = 1920, height = 1068, units = "px")
